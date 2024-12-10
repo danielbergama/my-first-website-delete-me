d8ef4daf58dd99dd900897c3e6c142e701e86a18
Daniele <daniele_bergamaschi@hotmail.com>
2024-12-09 16:30:59 -0800

    first site launch post error

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
 
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
-    <div className={styles.root}> 
-      
-      <img src="https://uploads8.wikiart.org/00340/images/raphael/the-school-of-atenas-1509-1510.jpg"> 
-      
-      
-      </img>
-      <div className={styles.imageText}>
-      this is whitespace
+    <div>
+      <header className={styles.header}>
+        <div className={styles.companyName}>My Company</div>
+        <nav className={styles.navButtons}>
+          <button>Services</button>
+          <button>Course</button>
+          <button>About</button>
+          <button>Contact</button>
+          <button>Book Now</button>
+        </nav>
+      </header>
+
+      <div className={styles.imageMain}>
+        <img className={styles.athens} src="https://uploads8.wikiart.org/00340/images/raphael/the-school-of-atenas-1509-1510.jpg" alt="The School of Athens" />
+        <div className={styles.imageText}>this is whitespace</div>
       </div>
-      
+
+
+
+
 
 
     </div>
+
+
   );
 }
